d47f35a640bd300b56d23b8588fda196104049ef
Dicka Ismaji <dickaismaji@gmail.com>
2022-04-02 22:07:51 +0700

    Feat: add avatar generator of name

---
+++ src/components/AvatarShortName/AvatarShortName.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  name: string;
+}
+
+const Avatar = styled.div`
+  border-radius: 50%;
+  padding: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  width: 42px;
+  height: 42px;
+  background-color: #0070f3;
+  color: white;
+`;
+
+const AvatarShortName:React.FC<Props> = ({ name }) => {
+  const shortName = name.split(' ')
+    .map((word) => word[0])
+    .join('')
+    .substring(0, 2)
+    .toUpperCase();
+
+  return (
+    <Avatar>
+      {shortName}
+    </Avatar>
+  );
+};
+
+export default AvatarShortName;
