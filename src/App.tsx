feb25c68c15898e888c235bbd96c7c72c01c556a
Dicka Ismaji <dickaismaji@gmail.com>
2022-04-05 15:47:34 +0700

    Feat: toaster, index, invoices, setting pages and protected route

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,23 @@ import {
 } from '@nextui-org/react';
 import useDarkMode from 'use-dark-mode';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Toaster } from 'react-hot-toast';
+
 import Index from './pages/index';
 import Projects from './pages/Projects';
 import ProjectDetail from './pages/ProjectDetail';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
+import Settings from './pages/Settings';
+import Invoices from './pages/Invoices';
+import EndpointDetail from './components/EndpointDetail/EndpointDetail';
+import EndpointCreate from './components/EndpointCreate/EndpointCreate';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 export const queryClient = new QueryClient();
 
-const App = () => {
+const App: React.FC = () => {
   const lightTheme = createTheme({
     type: 'light',
   });
@@ -28,13 +34,49 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <NextUIProvider theme={darkMode.value ? darkTheme : lightTheme}>
+        <Toaster />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route index element={<Index />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/projects/:projectName" element={<ProjectDetail />} />
+            <Route
+              path="/projects"
+              element={(
+                <ProtectedRoute>
+                  <Projects />
+                </ProtectedRoute>
+            )}
+            />
+            <Route
+              path="/projects/:prefixPath"
+              element={(
+                <ProtectedRoute>
+                  <ProjectDetail />
+                </ProtectedRoute>
+            )}
+            >
+              <Route path=":path" element={<EndpointDetail />} />
+              <Route path="create" element={<EndpointCreate />} />
+              <Route path="*" element={<span>Not found</span>} />
+            </Route>
+            <Route
+              path="/settings"
+              element={(
+                <ProtectedRoute>
+                  <Settings />
+                </ProtectedRoute>
+              )}
+            />
+            <Route
+              path="/invoices"
+              element={(
+                <ProtectedRoute>
+                  <Invoices />
+                </ProtectedRoute>
+              )}
+            />
+            <Route path="*" element={<span>Page is not found</span>} />
           </Routes>
         </BrowserRouter>
       </NextUIProvider>
