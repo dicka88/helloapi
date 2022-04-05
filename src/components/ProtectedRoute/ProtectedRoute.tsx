40c6220366bcb6c3bddf40d33a308f7e42b21bf5
Dicka Ismaji <dickaismaji@gmail.com>
2022-04-05 15:48:27 +0700

    Feat: protected route and zustand state management

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import useUser from '../../zustand/useUser';
+
+type Props = {
+  children: ReactElement
+}
+
+const ProtectedRoute: React.FC<Props> = ({ children }: Props) => {
+  const { user } = useUser();
+  const { pathname } = useLocation();
+
+  if (!user?.email) {
+    return (
+      <Navigate
+        to="/signin"
+        replace
+        state={{
+          from: { pathname },
+          message: 'You must signin first',
+        }}
+      />
+    );
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
---
+++ src/zustand/useUser.ts
@@ -0,0 +1,39 @@
+import create from 'zustand';
+import jwtDecode from 'jwt-decode';
+
+export type User = {
+  id?: string | null
+  email?: string | null
+  name?: string | null
+  avatarUrl?: string | null
+  exp?: number | null
+  iat?: number | null
+}
+
+let initialUser: User = {
+  id: null,
+  email: null,
+  name: null,
+  avatarUrl: null,
+  exp: null,
+  iat: null,
+};
+
+const token = localStorage.getItem('token');
+if (token) {
+  initialUser = jwtDecode(token) as User;
+}
+
+interface UserState {
+  user: User | null;
+  setUser: Function;
+  flush: () => void;
+}
+
+const useUser = create<UserState>((set) => ({
+  user: initialUser,
+  setUser: (user: User) => set((state) => ({ user: { ...state.user, ...user } })),
+  flush: () => set({ user: initialUser }),
+}));
+
+export default useUser;
