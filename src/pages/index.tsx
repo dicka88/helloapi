aca9c7b84bedf7e1fab0c24c6a34bf03878feb3e
Dicka Ismaji <dickaismaji@gmail.com>
2022-04-05 15:53:28 +0700

    Feat: Landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,163 @@
 import React from 'react';
+import {
+  Button, Container, Grid, Row, Spacer, Text,
+} from '@nextui-org/react';
+import { Link } from 'react-router-dom';
+import AceEditor from 'react-ace';
 
-const index: React.FC = () => (
-  <div>index</div>
+import Footer from '../components/Footer/Footer';
+import Seo from '../components/Seo/Seo';
+
+const Index: React.FC = () => (
+  <>
+    <Container lg>
+      <Seo
+        title="Hello API, Create your API without Coding"
+        description="Fake API Json with faker or just plain json"
+      />
+
+      <nav>
+        <Row justify="space-between" css={{ py: '$16' }}>
+          <Text h3>Hello API</Text>
+          <div>
+            <Row>
+              <Link to="/signin">
+                <Button auto css={{ marginRight: '$4' }}>Signin</Button>
+              </Link>
+              <Link to="/signup">
+                <Button auto light>Signup</Button>
+              </Link>
+            </Row>
+          </div>
+        </Row>
+      </nav>
+
+      <section>
+        <Grid.Container css={{ py: '$16', pb: '$24' }}>
+          <Grid xs={12} sm={6} md={6}>
+            <Row justify="center" align="center">
+              <div>
+                <Text h2 css={{ display: 'block' }}>
+                  Generate your API using
+                  {' '}
+                  <br />
+                  {' '}
+                  existing JSON
+                </Text>
+                <Spacer y={2} />
+                <AceEditor
+                  placeholder="Enter your JSON"
+                  mode="json"
+                  theme="xcode"
+                  name="blah2"
+                  fontSize="1rem"
+                  showPrintMargin={false}
+                  showGutter={false}
+                  highlightActiveLine={false}
+                  width="100%"
+                  height="200px"
+                  style={{
+                    padding: '5px',
+                    background: '#f4f4f4',
+                    borderRadius: '12px',
+                  }}
+                  onLoad={(editor: any) => {
+                    editor.renderer.setPadding(20);
+                    editor.renderer.setScrollMargin(20);
+                  }}
+                  value={JSON.stringify({ statusCode: 200, message: 'Hello API' }, null, '\t')}
+                  setOptions={{
+                    enableBasicAutocompletion: true,
+                    enableLiveAutocompletion: true,
+                    enableSnippets: true,
+                    showLineNumbers: false,
+                    tabSize: 2,
+                  }}
+                />
+                <Spacer y={1} />
+                <Row>
+                  <Link to="/signin">
+                    <Button auto css={{ marginRight: '$4' }}>
+                      Get Started
+                    </Button>
+                  </Link>
+                  <Button auto flat>
+                    Demo
+                  </Button>
+                </Row>
+              </div>
+            </Row>
+          </Grid>
+          <Grid xs={12} sm={6} md={6}>
+            <Row justify="center" align="center">
+              <img src="/illustrations/sammy-no-connection.gif" alt="HelloAPI" />
+            </Row>
+          </Grid>
+        </Grid.Container>
+      </section>
+
+      <section>
+        <Grid.Container css={{ py: '$24' }}>
+          <Grid xs={12} sm={4} md={4} justify="center" css={{ mb: '$12' }}>
+            <div>
+              <Text h4 css={{ marginBottom: '0.55em' }}>Built for developer</Text>
+              <Text>
+                Its free, serve any json data from anywhere,
+                <br />
+                any time, and its okay
+              </Text>
+            </div>
+          </Grid>
+          <Grid xs={12} sm={4} md={4} justify="center" css={{ mb: '$12' }}>
+            <div>
+              <Text h4 css={{ marginBottom: '0.55em' }}>Unlimited API to create</Text>
+              <Text>
+                Yes, its unlimited if you pay
+                <br />
+                Just kid, this is still beta so we didn't set any plan
+              </Text>
+            </div>
+          </Grid>
+          <Grid xs={12} sm={4} md={4} justify="center" css={{ mb: '$12' }}>
+            <div>
+              <Text h4 css={{ marginBottom: '0.55em' }}>Unlimited Bandwidth</Text>
+              <Text>
+                Why API is should limited ?
+                <br />
+                Break your limit :)
+              </Text>
+            </div>
+          </Grid>
+
+        </Grid.Container>
+      </section>
+
+      <section>
+        <Grid.Container css={{ py: '$24' }}>
+          <Grid sm={12} justify="center" css={{ mb: '$16' }}>
+            <Text h4>
+              Thanks for helping developer to code until deploy this site
+            </Text>
+          </Grid>
+          <Grid xs={6} sm={3} justify="center" css={{ mb: '$12' }}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" height={45} alt="" />
+          </Grid>
+          <Grid xs={6} sm={3} justify="center" css={{ mb: '$12' }}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/7/7b/Meta_Platforms_Inc._logo.svg" height={35} alt="" />
+          </Grid>
+          <Grid xs={6} sm={3} justify="center" css={{ mb: '$12' }}>
+            <img src="https://wizardsourcer.com/wp-content/uploads/2019/03/Stackoverflow.png" height={45} alt="" />
+          </Grid>
+          <Grid xs={6} sm={3} justify="center" css={{ mb: '$12' }}>
+            <img src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Emblem.png" height={45} alt="" />
+          </Grid>
+        </Grid.Container>
+      </section>
+
+    </Container>
+    <Footer />
+  </>
+
 );
 
-export default index;
+export default Index;
