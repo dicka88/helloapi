873b32490a60831fb4fe2e448c7a465ac8a2dedd
Dicka Ismaji <dickaismaji@gmail.com>
2022-04-05 16:38:13 +0700

    Update: Faker select component

--- src/components/FakerSelect/index.tsx
+++ src/components/FakerSelect/index.tsx
@@ -1,28 +1,91 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import Select, { SingleValue } from 'react-select';
 
-type Props = {
-  value: string
+export interface Faker {
+  label: string;
+  value?: string;
+  options?: Faker[];
 }
 
-const fakerOptions = [
-  'name.fullname',
-  'name.shortname',
+type Props = {
+  value?: string;
+  onChange: Function;
+  [x:string]: any;
+}
+
+const options: Faker[] = [
+  {
+    label: 'Address',
+    options: [
+      { label: 'Cardinal Direction', value: 'address.cardinalDirection' },
+      { label: 'City', value: 'address.city' },
+      { label: 'City Name', value: 'address.cityName' },
+      { label: 'City Prefix', value: 'address.cityPrefix' },
+      { label: 'City Suffix', value: 'address.citySuffix' },
+    ],
+  },
+  {
+    label: 'Animal',
+    options: [
+      { label: 'Bear', value: 'animal.bear' },
+      { label: 'Bird', value: 'animal.bird' },
+      { label: 'Cat', value: 'animal.cat' },
+      { label: 'Cetacean', value: 'animal.cetacean' },
+      { label: 'Cow', value: 'animal.cow' },
+    ],
+  },
+  {
+    label: 'Internet',
+    options: [
+      { label: 'Avatar', value: 'internet.avatar' },
+      { label: 'Color', value: 'internet.color' },
+      { label: 'Domain Name', value: 'internet.domainName' },
+      { label: 'Domain Suffix', value: 'internet.domainSuffix' },
+      { label: 'Domain Name', value: 'internet.domainName' },
+      { label: 'Email', value: 'internet.email' },
+      { label: 'Example Email', value: 'internet.exampleEmail' },
+    ],
+  },
 ];
 
-const Select = styled.select`
-  border-radius: 12px;
-  background: '#F0F0F0';
-  padding: 0.5em;
-  border: none;
-`;
+const getFlatOptions = (): Faker[] => {
+  let flattenOptions: Faker[] = [];
+  const flat = options.map((item: Faker) => item.options);
 
-const FakerSelect: React.FC<Props> = ({ value }) => (
-  <Select value={value}>
-    {fakerOptions.map((option) => (
-      <option key={option}>{option}</option>
-    ))}
-  </Select>
-);
+  flat.forEach((items) => {
+    flattenOptions = flattenOptions.concat([...items!]);
+  });
+
+  return flattenOptions;
+};
+
+const FakerSelect: React.FC<Props> = ({ value = '', onChange, ...props }) => {
+  const propValue = getFlatOptions().find((item: Faker) => item.value === value);
+  const [selectedValue, setSelectedValue] = useState<SingleValue<Faker>>(propValue as Faker);
+
+  const changeHandler = (option: SingleValue<Faker>) => {
+    onChange(option);
+    setSelectedValue(option);
+  };
+
+  return (
+    <Select<Faker>
+      placeholder="Faker options"
+      value={selectedValue}
+      options={options}
+      onChange={changeHandler}
+      styles={{
+        control: (provided: any) => ({
+          ...provided,
+          backgroundColor: '#fff',
+          border: '1px solid #ccc',
+          minWidth: '250px',
+          cursor: 'pointer',
+        }),
+      }}
+      {...props}
+    />
+  );
+};
 
 export default FakerSelect;
